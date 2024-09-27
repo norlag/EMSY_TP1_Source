7fe704430132bc34979718aef7e3e658868675d3
user <user@etml-es.ch>
2024-09-27 15:07:42 +0200

    Test EMSY TP1 fichier source

---
+++ EMSY_TP1.c
@@ -0,0 +1,26 @@
+//-----------------------------------------------------------------------------------//
+// Nom du projet 		: EMSY TP1 
+// Nom du fichier 		: EMSY_TP1.c 
+// Date de création 	: 27.09.2024
+// Date de modification : xx.xx.20xx
+//
+// Auteur 				: Philou (Ph. Bovey)
+//
+// Version				: 0.1
+//
+// Description          : 
+//
+//
+// Remarques :           
+//----------------------------------------------------------------------------------//
+
+//--- librairie standart ---//
+#include <stdio.h>	// pour usage printf et scanf_s
+                    // Nouveauté du VC++ 2005, 2008, 2010 et 2015 : le scanf_s remplace scanf
+
+//-- programme principale --// 
+int main(void)
+{
+
+  return(0);
+}
